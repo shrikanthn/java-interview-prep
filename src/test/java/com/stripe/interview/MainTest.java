a486a8f5b002dcfbfb858e5e52008b5941e876dc
Narayanan <shrikana@a07817a89c13.ant.amazon.com>
2021-09-07 09:35:22 -0700

    setting up fork branch

--- src/test/java/com/stripe/interview/MainTest.java
+++ src/test/java/com/stripe/interview/MainTest.java
@@ -3,8 +3,6 @@ package com.stripe.interview;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MainTest {
 	@Test
 	public void testUseGuavaForSomeReason() throws Exception {
